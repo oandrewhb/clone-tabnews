07b07427129f41aac20a4af41b2ee5ee521864b0
Andrew <andrew.contate@gmail.com>
2026-06-15 19:15:11 -0300

    add `database.js` and `.env` files

---
+++ infra/database.js
@@ -0,0 +1,20 @@
+import { Client } from 'pg';
+
+async function query(queryObject) {
+    const config = {
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        database: process.env.POSTGRES_DB,
+        password: process.env.POSTGRES_PASSWORD,
+    };
+    const client = new Client(config);
+    await client.connect();
+    const result = await client.query(queryObject);
+    await client.end();
+    return result;
+}
+
+export default {
+    query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-export default function status(request, response) {
+import database from "../../../../infra/database.js"
+
+export default async function status(request, response) {
+  const result = await database.query('select 1 + 1 as soma;');
+  console.log(result.rows);
   response.status(200)
   response.json({chave: 'top de mais essa api!'})
 }
