8920235fbc4d1d908aa08e4cd0549e803de730af
Andrew <andrew.contate@gmail.com>
2026-06-15 21:48:10 -0300

    add `jsconfig.js` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "../../../../infra/database.js"
+import database from "infra/database.js"
 
 export default async function status(request, response) {
   const result = await database.query('select 1 + 1 as soma;');
