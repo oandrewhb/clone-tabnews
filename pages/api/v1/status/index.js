b468287a711199df1157293151270b7f287b0e70
Andrew <andrew.contate@gmail.com>
2026-06-22 19:04:25 -0300

    format all files with `lint:fix` command

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import database from "infra/database.js"
+import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query('select 1 + 1 as soma;');
+  const result = await database.query("select 1 + 1 as soma;");
   console.log(result.rows);
-  response.status(200)
-  response.json({chave: 'top de mais essa api!'})
+  response.status(200);
+  response.json({ chave: "top de mais essa api!" });
 }
