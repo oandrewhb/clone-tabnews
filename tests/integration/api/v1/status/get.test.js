83e7450644e33eff30e713e87b608e09da24f351
Andrew <andrew.contate@gmail.com>
2026-06-23 08:06:43 -0300

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,44 @@
+const url = "http://localhost:3000/api/v1/status";
+
 test("GET to api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+  const response = await fetch(url);
   expect(response.status).toBe(200);
-  expect(response.statusText).toBe("OK");
+});
+
+test("GET to api/v1/status should return updated_at", async () => {
+  const response = await fetch(url);
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.updated_at).toBeTruthy();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+});
+
+test("GET to api/v1/status should return database version", async () => {
+  const response = await fetch(url);
+  const responseBody = await response.json();
+
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.version).toBeTruthy();
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+});
+
+test("GET to api/v1/status should return database max_connections", async () => {
+  const response = await fetch(url);
+  const responseBody = await response.json();
+
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  expect(responseBody.dependencies.database.max_connections).toBeTruthy();
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+});
+
+test("GET to api/v1/status should return database opened_connections", async () => {
+  const response = await fetch(url);
+  const responseBody = await response.json();
+
+  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
+  expect(responseBody.dependencies.database.opened_connections).toBeTruthy();
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
